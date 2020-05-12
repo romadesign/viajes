a1b23720cb0da003eaf78905409441a6769886b4
romadesign <roman247.rg@gmail.com>
2020-05-12 18:58:02 -0500

    first commit

---
+++ mail.php
@@ -0,0 +1,21 @@
+<?php
+if(isset( $_POST['name']))
+$name = $_POST['name'];
+if(isset( $_POST['email']))
+$email = $_POST['email'];
+if(isset( $_POST['message']))
+$message = $_POST['message'];
+if(isset( $_POST['subject']))
+$subject = $_POST['subject'];
+
+$content="From: $name \n Email: $email \n Message: $message";
+$recipient = "youremail@here.com";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $content, $mailheader) or die("Error!");
+echo "Email sent!";
+?>
+
+
+
+/**  Ahora solo reemplace youremail@here.com con su dirección de correo electrónico y listo. Recuerde que para que el script funcione deberá mantenerlo en un servidor compatible con PHP como Apache.
+ */
